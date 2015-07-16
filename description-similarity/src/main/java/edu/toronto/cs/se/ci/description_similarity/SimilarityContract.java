8ffc6acabd55cf8b3e14bb4f703a6a999b5ca9a2
wginsberg <will.j.ginsberg@gmail.com>
2015-07-16 09:20:35 -0400

    clean of repository

--- description-similarity/src/main/java/edu/toronto/cs/se/ci/description_similarity/SimilarityContract.java
+++
@@ -1,7 +0,0 @@
-package edu.toronto.cs.se.ci.description_similarity;
-
-import edu.toronto.cs.se.ci.Contract;
-
-public interface SimilarityContract extends Contract<SimilarityQuestion, Double, SimilarityTrust> {
-
-}
