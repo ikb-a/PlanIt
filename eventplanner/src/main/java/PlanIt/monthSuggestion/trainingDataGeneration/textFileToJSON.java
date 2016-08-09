c5e41442ce30d3f0d0bea7295000ab0b47ab4a91
Ian Berlot-Attwell <ian.berlot.attwell@mail.utoronto.ca>
2016-08-09 09:31:48 -0400

    Modified CI training data to have built in keywords

--- eventplanner/src/main/java/PlanIt/monthSuggestion/trainingDataGeneration/textFileToJSON.java
+++ eventplanner/src/main/java/PlanIt/monthSuggestion/trainingDataGeneration/textFileToJSON.java
@@ -6,6 +6,10 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class textFileToJSON {
 	public static final String FOLDER = "./src/main/resources/data/monthData/CI/";
@@ -30,15 +34,28 @@ public class textFileToJSON {
 	public static void textToJSON(String path) throws IOException {
 		File f = new File(path);
 		String result = "{\n\"events\" : [";
-		String template = "\u0009{\n\u0009\"title\" : \"%s\",\n\u0009\"description\" : \"%s\",\n\u0009\"venue\" : {\n\u0009\u0009\"address\" : {\n\u0009\u0009\u0009\"country\" : \"%s\"\n\u0009\u0009\u0009}\n\u0009\u0009}\n\u0009},";
+		String template = "\n\u0009{\n\u0009\"title\" : \"%s\",\n\u0009\"description\" : \"%s\",\n\u0009\"keyWords\":[%s],\n\u0009\"venue\" : {\n\u0009\u0009\"address\" : {\n\u0009\u0009\u0009\"country\" : \"%s\"\n\u0009\u0009\u0009}\n\u0009\u0009}\n\u0009},";
 		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
 		String line = br.readLine();
 		while (line != null) {
 			String name = line.replaceAll("\"", "");
 			String desc = br.readLine().replaceAll("\"", "");
+			String keywordsAsString = br.readLine().replaceAll("\"", "");
+			List<String> keywords = new ArrayList<String>();
+
+			Pattern p = Pattern.compile("\\('(.*?)', \\d, \\d\\)");
+			Matcher m = p.matcher(keywordsAsString);
+			while (m.find()) {
+				String keyword = m.group(1);
+				keyword = keyword.replaceAll(" \\x92", "'");
+				keywords.add("\"" + keyword + "\"");
+			}
+			keywordsAsString = keywords.toString();
+			keywordsAsString = keywordsAsString.substring(1, keywordsAsString.length() - 1);
+
 			String country = br.readLine();
 			assert (country != null);
-			result += String.format(template, name, desc, country);
+			result += String.format(template, name, desc, keywordsAsString, country);
 			line = br.readLine();
 		}
 		br.close();
